f6683eae6c86033a90991dc069e2906cb75c0c68
Angelo Nicolo <AngeloCNicolo@gmail.com>
2023-06-05 23:07:00 -0700

    update

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -9,18 +9,36 @@
 
     let myDate = new Date();
 
+   */
+   
+    function coffeeTemplate(){
+        return <p>
+        <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+        <strong class="feature">${coffee.day} Coffee Special:</strong> ${coffee.day} daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
+        ;
+    }
+
     let today = myDate.getDay();
     
-    //test today = 3;
+    test today = 3;
     
     switch(today){
-    
+        case 1:
+            today = "Monday";
+        let coffee = {
+            name: "Bubble Tea",
+            pic: "images/bubble-tea.jpg",
+            alt: "A picture of a bubble tea",
+            color: "pink",
+            day: "Wednesday",
+            desc: `I love me some bubble tea!`
+        };
         case 2:
             today = "Tuesday";
         break;
     
         case 3:
-            today = "Wednesday";
+            today = "wednesday";
         break;
     
         default:
@@ -34,6 +52,10 @@
         color: "pink",
         day: "Wednesday",
         desc: `I love me some bubble tea!`
-    }
+    };
     
     console.log(coffee);
+
+    document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+    //alert(today)
