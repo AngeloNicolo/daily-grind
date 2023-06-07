1010b2f4df5e730d02c915036348a004be1b9d25
Angelo Nicolo <AngeloCNicolo@gmail.com>
2023-06-06 21:54:24 -0700

    update

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -6,56 +6,127 @@
     day - the day of the week
     desc - description of the coffee
     */
-
-    let myDate = new Date();
-
-   */
    
-    function coffeeTemplate(){
-        return <p>
+    function coffeeTemplate() {
+        return `<p>
         <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
         <strong class="feature">${coffee.day} Coffee Special:</strong> ${coffee.day} daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
-        ;
+        `;
     }
-
+    
+    let myDate = new Date();
+    
+    let coffee = "";
+    
     let today = myDate.getDay();
     
-    test today = 3;
+    //use location object to access querystring (address bar)
+    const queryString = window.location.search;
+        
+    //output to console    
+    console.log(queryString);
+        
+    //separate query string parameters
+    const urlParams = new URLSearchParams(queryString);
     
-    switch(today){
-        case 1:
-            today = "Monday";
-        let coffee = {
+    //today = 3;
+    
+    if(urlParams.has("day")){//from querystring
+        today = urlParams.get("day");
+     }
+    
+today = parseInt(today);
+
+switch(today){
+
+    case 0:
+        today = "Sunday";
+        coffee = {
+        name: "Pumkin Spice Latte",
+        pic: "images/pumpkin-spice-latte.jpg",
+        alt: "A picture of a pumkin-spice-latte",
+        color: "orange",
+        day: "Sunday",
+        desc: `I love me some bubble tea!`
+        }; 
+        break;       
+    case 1:
+        today = "Monday";
+        coffee = {
             name: "Bubble Tea",
             pic: "images/bubble-tea.jpg",
             alt: "A picture of a bubble tea",
             color: "pink",
-            day: "Wednesday",
+            day: "Monday",
             desc: `I love me some bubble tea!`
         };
-        case 2:
-            today = "Tuesday";
         break;
-    
-        case 3:
-            today = "wednesday";
+    case 2:
+        today = "Tuesday";
+        coffee = {
+            name: "Drip",
+            pic: "images/drip.jpg",
+            alt: "A picture of a drip coffee",
+            color: "black",
+            day: "Tuesday",
+            desc: `Give it to me straight!`
+        };
+        break;
+    case 3:
+        today = "Wednesday";
+        coffee = {
+            name: "Caramel-Latte",
+            pic: "images/caramel-latte.jpg",
+            alt: "A picture of a bubble tea",
+            color: "brown",
+            day: "Wednesday",
+            desc: `I love me some Caramel-Latte!`
+        };
+        break; 
+    case 4:
+        today = "Thursday";
+        coffee = {
+            name: "Mocha",
+            pic: "images/mocha.jpg",
+            alt: "A picture of a mocha coffee",
+            color: "light brown",
+            day: "Thursday",
+            desc: `I love me some mocha!`
+        }; 
+        break;
+    case 5:
+        today = "Friday";
+        coffee = {
+            name: "Cold-Brew",
+            pic: "images/cold-brew.jpg",
+            alt: "A picture of a cold-brew coffee",
+            color: "blue",
+            day: "Friday",
+            desc: `I love me some bubble tea!`
+        };
+        break;
+    case 6:
+        today = "Saturday";
+        coffee = {
+            name: "",
+            pic: "images/largebucks-logo.jpg",
+            alt: "A picture of a largebucks-logo",
+            color: "red",
+            day: "Saturday",
+            desc: `I love me coffee!`
+        };
         break;
     
         default:
             today ="Day is unknown!";
+
     }
     
-    let coffee = {
-        name: "Bubble Tea",
-        pic: "images/bubble-tea.jpg",
-        alt: "A picture of a bubble tea",
-        color: "pink",
-        day: "Wednesday",
-        desc: `I love me some bubble tea!`
-    };
-    
-    console.log(coffee);
 
     document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+    document.querySelector("html").style.backgroundColor = coffee.color;
+    
+    //alert(today);
+    console.log(coffee);
 
     //alert(today)
